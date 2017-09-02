9e5b7e50c0baf04836f666968428bbde7de026be
EC2 Default User <ec2-user@ip-172-31-29-74.us-west-1.compute.internal>
2017-09-02 03:28:43 +0000

    For Loop corrected

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        for (i=1;i<=5;1++){
+        for (int i=1;i<=5;1++){
         System.out.println("Hello, World!");
     }
 }
